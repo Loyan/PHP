513991d006a61ebeb6c1ee9ae1bab9e56c5eb7e5
Loyan <loyan93@mail.ru>
2014-05-28 11:48:20 +0300

    Create add1.php

---
+++ add1.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>shirts</title>
+</head>
+<body>
+<h1> shirt's table</h1>
+<ul>
+  <li><a href="create.php">create table in db</a></li>
+  <li><a href="add.php">add shirt's data</a></li>
+  <li><a href="list.php">list shirt's data</a></li>
+</ul>
+</body>
+</html>
