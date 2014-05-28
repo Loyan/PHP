ab24902a7d872045baf8be3070c8e492c90904ec
Loyan <loyan93@mail.ru>
2014-05-28 12:10:56 +0300

    Update edit1.php

--- edit1.php
+++ edit1.php
@@ -9,25 +9,22 @@ echo "<p>size=$size</p>";
 echo "<p>color=$color</p>";
 echo "<p>material=$material</p>";
 
-mysql_connect("localhost","student2","111");
-mysql_query("use shirt's");
-$query = "update shirt's set brand='$brand' where id=$id";
+mysql_connect("localhost","-u root"," ");
+mysql_query("use shirts");
+$query = "update shirts set brand='$brand' where id=$id";
 echo "q=$query";
 mysql_query($query);
-$query = "update shirt's set size='$size' where id=$id";
+$query = "update shirts set size='$size' where id=$id";
 mysql_query($query);
 
-mysql_query("use shirt's");
+mysql_query("use shirts");
 echo "q=$query";
-$query = "update shirt's set color='$color' where id=$id";
+$query = "update shirts set color='$color' where id=$id";
 mysql_query($query);
 
-mysql_query("use shirt's");
-$query = "update shirt's set material='$material' where id=$id";
+mysql_query("use shirts");
+$query = "update shirts set material='$material' where id=$id";
 echo "q=$query";
 mysql_query($query);
-
-//$query = "update shirt's set (brand,size,color,material) 
-//	values ('$brand', '$size','$color','$material');";
 
 ?>
